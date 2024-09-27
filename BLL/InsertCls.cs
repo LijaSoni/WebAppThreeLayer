a40d014e55a1cf08906c642b58fa645147f73291
LijaSoni <lijasalim@gmail.com>
2024-09-27 16:21:32 +0530

    Add project files.

---
+++ BLL/InsertCls.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+namespace BLL
+{
+    public class InsertCls
+    {
+        ConnectionCls objdal = new ConnectionCls();
+        public int DBInsert(string na, int ag, string addr, string ph, string una, string pwd)
+        {
+            string ins = "insert into Reg values('" + na + "'," + ag + ",'" + addr + "','" + ph + "','" + una + "','" + pwd + "')";
+            int i = objdal.Fun_exenonquery(ins);
+            return i;
+        }
+    }
+}
